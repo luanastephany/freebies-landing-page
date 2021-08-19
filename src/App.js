051fb8bcff38fd832cd72cbce3f7a732f7fd5999
Luana Stephany <ls.luanastephany@gmail.com>
2021-08-19 09:38:02 -0300

    atualizando raiz

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import './App.scss';
 import Header from './components/Header'
 import About from './components/About'
+import Estatistics from './components/Estatistics';
+import Features from './components/Features';
+import Plans from './components/Plans/Plans';
 
 function App() {
   return (
     <main>
       <Header />
       <About />
+      <Estatistics />
+      <Features />
+      <Plans />
 
     </main>
 
