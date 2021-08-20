83720a9dcf07406c4af37a15f1aa69c90afbef36
Luana Stephany <ls.luanastephany@gmail.com>
2021-08-20 09:08:47 -0300

    componente dos planos

--- src/components/Plans/Plans.js
+++ src/components/Plans/Plans.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import './Plans.scss'
-
+import { FaCheck } from 'react-icons/fa'
+import ButtonRound from '../ButtonRound'
 
 const Plan = ({ image, title, price, benefits }) => {
   return (
@@ -9,17 +10,21 @@ const Plan = ({ image, title, price, benefits }) => {
 
       <div className="texts">
         <p>{title}</p>
-        {benefits.map(item => (
-          <div>
-            ICONE -
-            <span>{item}</span>
+
+        {benefits.map(benefit => (
+          <div className="list">
+            <FaCheck color="#2FAB73" />
+            <span>{benefit}</span>
           </div>
         ))}
+
       </div>
 
       <div className="prices">
-        <h1>{price}</h1>
+        <h1 className="price">{price}</h1>
       </div>
+
+      <ButtonRound>Select</ButtonRound>
 
     </div>
   )
@@ -28,8 +33,8 @@ const Plan = ({ image, title, price, benefits }) => {
 const Plans = () => {
   return (
     <div className="plans">
-      <h1>Choose Your Plan</h1>
-      <p>Let's choose the package that is best for you and explore it happily and cheerfully.</p>
+      <h1 className="plan-title">Choose Your Plan</h1>
+      <p className="sub">Let's choose the package that is best for you and explore it happily and cheerfully.</p>
 
       <div className="types">
         <Plan
@@ -43,16 +48,32 @@ const Plans = () => {
             "Works on All Devices",
           ]}
         />
+
         <Plan
           image="/img/Standard.png"
           title="Standard Plan"
           price="$9/mo"
-          benefits={[]} />
+          benefits={[
+            "Unlimited Bandwitch",
+            "Encrypted Connection",
+            "No Traffic Logs",
+            "Works on All Devices",
+            "Connect Anyware",
+          ]}
+        />
+
         <Plan
           image="/img/Premium.png"
           title="Premium Plan"
           price="$12/mo"
-          benefits={[]} />
+          benefits={[
+            "Unlimited Bandwitch",
+            "Encrypted Connection",
+            "No Traffic Logs",
+            "Works on All Devices",
+            "Connect Anyware",
+            "Get New Features",
+          ]} />
       </div>
 
     </div>
