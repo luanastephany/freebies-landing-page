2b8f11d3b750dadd636e943411a70629d2286be1
Luana Stephany <ls.luanastephany@gmail.com>
2021-08-22 22:14:38 -0300

    atualizando pastas

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.scss';
-import Header from './components/Header'
-import About from './components/About'
+import Header from './components/Header';
+import About from './components/About';
 import Estatistics from './components/Estatistics';
 import Features from './components/Features';
 import Plans from './components/Plans/Plans';
 import Locations from './components/Locations';
+import Testimonials from './components/Testimonials/Testimonials';
+import Subscription from './components/Subscription/Subscription';
 import Footer from './components/Footer/Footer';
 
 
@@ -17,6 +19,8 @@ function App() {
       <Features />
       <Plans />
       <Locations />
+      <Testimonials />
+      <Subscription />
       <Footer />
 
     </main>
